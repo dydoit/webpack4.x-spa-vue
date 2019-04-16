ef8cdef4068d9c464a04e71edf0dd32c1c33c3da
dengyin <18565125218@163.com>
2019-04-16 22:59:00 +0800

    init

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,1 +1,12 @@
 const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, '../src/main.js')
+  },
+  output: {
+    path: resolve(dist)
+  }
+}
